8d3f8dc1fd395bc3d0939bca345c153d91181726
Dimebagg92 <nos1247@gmail.com>
2018-03-23 17:53:41 +0900

    signal generator

---
+++ plotter.py
@@ -0,0 +1,18 @@
+
+
+import numpy as np
+import matplotlib.pyplot as plt
+
+# describe the function below
+
+t_ax = [1]
+sig = [1]
+i=1
+while i<10:
+    t_ax.append(i)
+    sig.append(0)
+    i=i+1
+
+plt.stem(sig)
+plt.show()
+print(sig)
